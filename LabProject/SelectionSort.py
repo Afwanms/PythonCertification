205729f60a43eee14f4b29ac3fe99e61116d559b
Afwan Maulana Sidqi <afwanmaulanas02@gmail.com>
2026-03-10 14:17:11 +0700

    Edit Lab Project #11

--- LabProject/SelectionSort.py
+++ LabProject/SelectionSort.py
@@ -4,9 +4,9 @@ def selection_sort(array):
     
     for num in range(len(array)):
         min_index = num
-        for j in range(num + 1, len(array)):
-            if array[j] < array[min_index]:
-                min_index = j
+        for value in range(num + 1, len(array)):
+            if array[value] < array[min_index]:
+                min_index = value
         if min_index != num:
             array[num], array[min_index] = array[min_index], array[num]
     return array
