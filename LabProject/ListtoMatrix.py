11f8c28968aaf14980df99d403c9958025e44a5c
Afwan Maulana Sidqi <afwanmaulanas02@gmail.com>
2026-03-13 15:12:02 +0700

    Add Lab Project #13

---
+++ LabProject/ListtoMatrix.py
@@ -0,0 +1,15 @@
+def adjacency_list_to_matrix(dictionary):
+    adj_matrix = []
+    for key in dictionary:
+        row = [0] * len(dictionary)
+        for neighbor in dictionary[key]:
+            row[neighbor] = 1
+        adj_matrix.append(row)
+    
+    # Print each row in the adjacency matrix
+    for row in adj_matrix:
+        print(row)
+    
+    return adj_matrix
+
+print(adjacency_list_to_matrix({0: [1, 2], 1: [2], 2: [0, 3], 3: [2]}))
