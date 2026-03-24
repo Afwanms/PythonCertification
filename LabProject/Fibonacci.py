47c7061d6631153d0920eae868a71dbef1785ffe
Afwan Maulana Sidqi <afwanmaulanas02@gmail.com>
2026-03-24 14:22:27 +0700

    Add Lab Project #16

---
+++ LabProject/Fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    sequence = [0, 1]
+    
+    for i in range(2, n + 1):
+        sequence.append(sequence[i - 1] + sequence[i - 2])
+    
+    return sequence[n]
